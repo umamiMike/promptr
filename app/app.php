c156bcb8e461e92085231086a1fab3538adecc81
Ian Mckenney & Mike Chastain <student@epicodus.com>
2015-09-01 11:40:21 -0700

    merge topicsandpromptrs branch

--- app/app.php
+++ app/app.php
@@ -9,7 +9,9 @@
 
     $app = new Silex\Application();
     $app['debug']  = true;
-    $app->register(new Silex\Provider\TwigServiceProvider(),array('twig.path' => __DIR__.'/../views'));
+    $app->register(new Silex\Provider\TwigServiceProvider(),array(
+        'twig.path' => __DIR__.'/../views'
+    ));
 
     $app->get("/", function() use ($app){
       $mystring = "Data from the app display in twig";
