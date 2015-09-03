c5c5ea9cacc6fbb3e895b0aa29ebc0e236482d64
JackMackerel <ludichrislyts@gmail.com>
2015-09-03 01:15:57 -0700

    get admin route closer to working

--- app/app.php
+++ app/app.php
@@ -27,10 +27,37 @@
 // this route is manually entered and used only to populate the database
     $app->get("/admin", function() use ($app){
         $topics = Topic::getAll();
-        $promptrs = Promptr::getAll();
+        $all_promptrs = Promptr::getAll();
+        // find promptrs that need questions (newly added -- need to do this to get promptr id)
+        $empty_promptrs = [];
+        foreach($all_promptrs as $promptr){
+            if(empty($promptr->getQuestions())){
+                array_push($empty_promptrs, $promptr);
+            }
+        }
+        
         return $app['twig']->render('promptr-admin.html.twig', array(
                                     'topics' => $topics, 
-                                    'promptrs' => $promptrs));
+                                    'promptrs' => $empty_promptrs));
+    });
+// renders after adding a blank promptr
+    $app->post("/admin", function() use ($app){
+        $promptr_name = $_POST['promptr_name'];
+        $topic_id = $_POST['topic_id'];
+        $new_promptr = new Promptr($promptr_name, $topic_id);
+        $new_promptr->save();
+        $topics = Topic::getAll();
+        $all_promptrs = Promptr::getAll();
+        // find promptrs that need questions (newly added -- need to do this to get promptr id)
+        $empty_promptrs = [];
+        foreach($all_promptrs as $promptr){
+            if(empty($promptr->getQuestions())){
+                array_push($empty_promptrs, $promptr);
+            }
+        }        
+        return $app['twig']->render('promptr-admin.html.twig', array(
+                                    'topics' => $topics, 
+                                    'promptrs' => $empty_promptrs));
     });
 // //Admin page after all promptr delete -- refreshes admin page with topics only
     $app->delete("/admin", function() use ($app){
