058b0aa36d0eed43d2690ea249adac8f8ac1755e
umamiMike <michael.w.chastain@gmail.com>
2015-12-18 13:21:10 -0800

    alterations for deploy

--- app/config.php
+++ app/config.php
@@ -1,5 +1,5 @@
 <?php
-$server = 'mysql:host=localhost;dbname=promptr_app';
+$server = 'mysql:host=localhost;dbname=umamimed_promptr_app';
 $username = 'balls';
 $password = 'face';
 $DB = new PDO($server, $username, $password);
